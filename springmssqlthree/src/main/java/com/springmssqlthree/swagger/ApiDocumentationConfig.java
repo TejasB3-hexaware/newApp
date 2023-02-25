b869069aaca3a44578c3a1fb9dc40d559c522fe5
TejasB3-hexaware <1000070966@hexaware.com>
2023-02-25 10:20:52 +0000

    push changes

---
+++ springmssqlthree/src/main/java/com/springmssqlthree/swagger/ApiDocumentationConfig.java
@@ -0,0 +1,31 @@
+package com.springmssqlthree.swagger;
+
+import io.swagger.annotations.Contact;
+import io.swagger.annotations.ExternalDocs;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.License;
+import io.swagger.annotations.SwaggerDefinition;
+
+@SwaggerDefinition(
+        info = @Info(
+                description = "Springmssqlthree Resources",
+                version = "V12.0.12",
+                title = "Springmssqlthree Resource API",
+                contact = @Contact(
+                   name = "Springmssqlthree Team", 
+                   email = "rapidx@hexaware.com", 
+                   url = "http://www.hexaware.com"
+                ),
+                license = @License(
+                   name = "Springmssqlthree 2.0", 
+                   url = "http://www.hexaware.com/licenses/LICENSE-2.0"
+                )
+        ),
+        consumes = {"application/json", "application/xml"},
+        produces = {"application/json", "application/xml"},
+        schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS},
+        externalDocs = @ExternalDocs(value = "Read This For Sure", url = "http://www.springmssqlthree.org")
+)
+public interface ApiDocumentationConfig {
+
+}
