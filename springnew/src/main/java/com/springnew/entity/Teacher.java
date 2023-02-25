8fc87f593fdaa98331b3824bdbfeed0098b31d93
TejasB3-hexaware <111104658+TejasB3-hexaware@users.noreply.github.com>
2023-02-25 10:51:15 +0530

    push teacher

--- springnew/src/main/java/com/springnew/entity/Teacher.java
+++ springnew/src/main/java/com/springnew/entity/Teacher.java
@@ -30,18 +30,18 @@ public class Teacher {
 	private Long id;
     private String name;
 
-	public Teacher(String id, 
+	public Teacher(Long id, 
         String name
     ){
     this.id = id;
 	this.name = name;
 	}
 
-    public String getId() {
+    public Long getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
