d3bb7dbef8b08a7b529192989d5ed0d119ac3df2
TejasB3-hexaware <111104658+TejasB3-hexaware@users.noreply.github.com>
2023-02-25 11:27:37 +0530

    test file psh

--- springnew/src/test/java/com/springnew/controller/TeacherControllerTest.java
+++ springnew/src/test/java/com/springnew/controller/TeacherControllerTest.java
@@ -27,28 +27,24 @@ public class TeacherControllerTest {
 	private TeacherController controller;
 	
 	private List<Teacher> prepareTeacherRecords(){
-		List<Teacher> teacherstudents = new ArrayList<Teacher>();
-		Teacher teacherstudent1 = new Teacher(1L, "Sabari", "sabari@fff.com", "Nathan", "H23423443", 21, false, 232342345345f);
-		Teacher teacherstudent2 = new Teacher(1L, "Kishore", "kishore@fff.com", "kumar", "H26563463", 27, true, 345677345345f);
-		teacherstudents.add(teacherstudent1);
-		teacherstudents.add(teacherstudent2);
-		return teacherstudents;
+		List<Teacher> teacher = new ArrayList<Teacher>();
+		Teacher teacher = new Teacher(1L, "Sabari");
+		Teacher teacherstudent2 = new Teacher(1L, "Kishore");
+		teacherstudents.add(teacher1);
+		teacherstudents.add(teacher2);
+		return teacher;
 	}
 	
 	@Test
 	public void testFetchAllPass() {
 		Mockito
         .when(controller.fetchAll()).thenReturn(prepareTeacherRecords());
-		List<Teacher> teacherstudents = prepareTeacherRecords();
-		List<Teacher> teacherstudentsFromController =  controller.fetchAll();
-		for(int i=0; i<teacherstudents.size();i++) {
-			Assertions.assertEquals(teacherstudents.get(i).getId(), teacherstudentsFromController.get(i).getId());
-			Assertions.assertEquals(teacherstudents.get(i).getAge(), teacherstudentsFromController.get(i).getAge());
-			Assertions.assertEquals(teacherstudents.get(i).getPlateletsCount(), teacherstudentsFromController.get(i).getPlateletsCount());
-			Assertions.assertEquals(teacherstudents.get(i).getEmailId(), teacherstudentsFromController.get(i).getEmailId());
-			Assertions.assertEquals(teacherstudents.get(i).getFirstName(), teacherstudentsFromController.get(i).getFirstName());
-			Assertions.assertEquals(teacherstudents.get(i).getLastName(), teacherstudentsFromController.get(i).getLastName());
-			Assertions.assertEquals(teacherstudents.get(i).getPassportNumber(), teacherstudentsFromController.get(i).getPassportNumber());
+		List<Teacher> teacher = prepareTeacherRecords();
+		List<Teacher> teacherFromController =  controller.fetchAll();
+		for(int i=0; i<teacher.size();i++) {
+			Assertions.assertEquals(teacher.get(i).getId(), teacherFromController.get(i).getId());
+			Assertions.assertEquals(teacher.get(i).getName(), teacherFromController.get(i).getName());
+			
 		}
 		
 	}
@@ -57,9 +53,9 @@ public class TeacherControllerTest {
 	public void testFetchAllFailure() {
 		Mockito
         .when(controller.fetchAll()).thenReturn(prepareTeacherRecords());
-		List<Teacher> teacherstudents = null; //Intentionally made null to fail the test.
-		List<Teacher> teacherstudentsFromController =  controller.fetchAll();
-		Assertions.assertNotEquals(teacherstudents, teacherstudentsFromController);
+		List<Teacher> teacher = null; //Intentionally made null to fail the test.
+		List<Teacher> teacherFromController =  controller.fetchAll();
+		Assertions.assertNotEquals(teacher, teacherFromController);
 	}
 	
 	
@@ -70,13 +66,6 @@ public class TeacherControllerTest {
 			Teacher teacherByIdFromController = controller.fetchById(1L);
 			
 			Assertions.assertEquals(teacherById.getId(), teacherByIdFromController.getId());
-			Assertions.assertEquals(teacherById.getAge(), teacherByIdFromController.getAge());
-			Assertions.assertEquals(teacherById.getPlateletsCount(), teacherByIdFromController.getPlateletsCount());
-			Assertions.assertEquals(teacherById.getEmailId(), teacherByIdFromController.getEmailId());
-			Assertions.assertEquals(teacherById.getFirstName(), teacherByIdFromController.getFirstName());
-			Assertions.assertEquals(teacherById.getLastName(), teacherByIdFromController.getLastName());
-			Assertions.assertEquals(teacherById.getPassportNumber(), teacherByIdFromController.getPassportNumber());
-		     
 			Assertions.assertEquals(teacherById.getName(), teacherByIdFromController.getName());
 	 }
 
@@ -87,14 +76,7 @@ public class TeacherControllerTest {
 			Teacher teacherByIdFromController = controller.fetchById(1L);
 			
 			Assertions.assertNotEquals(teacherById.getId(), teacherByIdFromController.getId());
-			Assertions.assertNotEquals(teacherById.getAge(), teacherByIdFromController.getAge());
-			Assertions.assertNotEquals(teacherById.getPlateletsCount(), teacherByIdFromController.getPlateletsCount());
-			Assertions.assertNotEquals(teacherById.getEmailId(), teacherByIdFromController.getEmailId());
-			Assertions.assertNotEquals(teacherById.getFirstName(), teacherByIdFromController.getFirstName());
-			Assertions.assertNotEquals(teacherById.getLastName(), teacherByIdFromController.getLastName());
-			Assertions.assertNotEquals(teacherById.getPassportNumber(), teacherByIdFromController.getPassportNumber());
-
-        Assertions.assertNotEquals(teacherById.getName(), teacherByIdFromController.getName());
+       		Assertions.assertNotEquals(teacherById.getName(), teacherByIdFromController.getName());
 	 }
 	 
 	 @Test
@@ -114,12 +96,6 @@ public class TeacherControllerTest {
 		
 		Teacher teacherFromController  = controller.create(teacherToBeCreated);
 		Assertions.assertNotEquals(teacherToBeCreated.getId(), teacherFromController.getId()); //Since Id of created one is mocked as changed from within serviceid, it cannot be equal. 
-		Assertions.assertEquals(teacherToBeCreated.getAge(), teacherFromController.getAge());
-		Assertions.assertEquals(teacherToBeCreated.getPlateletsCount(),teacherFromController.getPlateletsCount());
-		Assertions.assertEquals(teacherToBeCreated.getEmailId(), teacherFromController.getEmailId());
-		Assertions.assertEquals(teacherToBeCreated.getFirstName(), teacherFromController.getFirstName());
-		Assertions.assertEquals(teacherToBeCreated.getLastName(), teacherFromController.getLastName());
-		Assertions.assertEquals(teacherToBeCreated.getPassportNumber(),teacherFromController.getPassportNumber());
         Assertions.assertEquals(teacherToBeCreated.getName(), teacherFromController.getName());
 	}
 	
